96adea2e8290edd9066c4c48ddbc04b590b4d7fc
Liao Zhu <liao.zhu.cs@gmail.com>
2022-09-15 02:55:41 -0500

    Updated load user

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ login_manager.login_view = "login"
 
 @login_manager.user_loader
 def load_user(user_id):
-    return Users.query.get(user_id)
+    return Users.query.get(int(user_id))
 
 # ===== Routes =====
 @app.route("/")
