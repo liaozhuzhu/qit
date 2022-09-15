7526eda3cb64c72ac12c4f6485aa916cdc0fb028
Liao Zhu <liao.zhu.cs@gmail.com>
2022-09-15 16:02:22 -0500

    brought back likes

--- static/index.js
+++ static/index.js
@@ -1,16 +1,16 @@
-// function like(postId) {
-//     const likeCount = document.getElementById(`likes-count-${postId}`);
-//     const likeButton = document.getElementById(`like-button-${postId}`);
+function like(postId) {
+    const likeCount = document.getElementById(`likes-count-${postId}`);
+    const likeButton = document.getElementById(`like-button-${postId}`);
   
-//     fetch(`/like/${postId}`, { method: "POST" })
-//       .then((res) => res.json())
-//       .then((data) => {
-//         likeCount.innerHTML = data["likes"];
-//         if (data["liked"] === true) {
-//           likeButton.className = "fa-solid fa-heart";
-//         } else {
-//           likeButton.className = "fa-regular fa-heart";
-//         }
-//       })
-//       .catch((e) => alert("Could not like post."));
-//   }
+    fetch(`/like/${postId}`, { method: "POST" })
+      .then((res) => res.json())
+      .then((data) => {
+        likeCount.innerHTML = data["likes"];
+        if (data["liked"] === true) {
+          likeButton.className = "fa-solid fa-heart";
+        } else {
+          likeButton.className = "fa-regular fa-heart";
+        }
+      })
+      .catch((e) => alert("Could not like post."));
+  }
