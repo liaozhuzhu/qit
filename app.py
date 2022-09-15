2914889becab57705f0feb0ba163def97e4aeca4
Liao Zhu <liao.zhu.cs@gmail.com>
2022-09-15 15:49:02 -0500

    flask login

--- app.py
+++ app.py
@@ -31,11 +31,11 @@ migrate = Migrate(app, db)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = "login"
+login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
-    return Users.query.get(int(user_id))
+	return Users.query.get(int(user_id))
 
 # ===== Routes =====
 @app.route("/")
